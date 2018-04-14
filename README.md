46f68a9bc6f8976ecd885497281bb44e43b27f4c
Taylor Burgess <tayloraburgess@gmail.com>
2018-04-14 18:39:00 -0400

    Remove Jekyll; use static site content instead

--- README.md
+++
@@ -1,5 +0,0 @@
-# tayloraburgess.github.io
-
-## This is a blog/portfolio static site.
-
-It uses [Jekyll](http://jekyllrb.com/).
--- _includes/blog.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-  Array.prototype.slice.call(document.getElementsByClassName('tag-or-year')).forEach(function(element) {
-    element.addEventListener('click', function() {
-      element.classList.toggle('show');
-    });
-  });
-});
